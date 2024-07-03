03e51b6dbaa30678d4184fad3dc0d0a7438c27dd
msa-sadeghi <sadeghi.msai@gmail.com>
2024-07-03 18:42:21 +0330

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,43 @@
+#print("hello")
+
+# score = 20
+# print(score)
+# print(type(score))
+
+# name = "artin"
+# print(name)
+# print(type(name))
+
+# a = 12
+# b = 13
+
+# z = a + b
+# print(z)
+
+# name = "artin"
+# family = "ahmadi"
+# print(name + family)
+
+# a = int(input("enter a number: "))
+# b = int(input("enter another number: "))
+# z = a + b
+# print(z)
+
+
+# a = "12"
+# b = "13"
+# z = a + b
+# print(z)
+
+#  +     -      *      /
+# تمرین
+# برنامه ای بنویسید که سه عدد از ورودی دریافت نماید
+# و حاصلجمع دو عدد اول را در عدد سوم ضرب نماید
+# برنامه ای بنویسید که نام و نام خانوادگی فردی را از کاربر ورودی دریافت نماید
+# و اطلاعات داده شده ار با فاصه نمایش دهد
+# artin rezaei
+
+
+
+
+
