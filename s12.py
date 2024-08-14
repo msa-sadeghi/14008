3523b82e10b3eca646fb8d641df50da7ce7c4372
msa-sadeghi <sadeghi.msai@gmail.com>
2024-08-14 18:26:16 +0330

    Create s12.py

---
+++ s12.py
@@ -0,0 +1,53 @@
+# heights = []
+# h1 = float(input("enter a h: "))
+# h2 = float(input("enter a h: "))
+# h3 = float(input("enter a h: "))
+# h4 = float(input("enter a h: "))
+# h5 = float(input("enter a h: "))
+# heights.append(h1)
+# heights.append(h2)
+# heights.append(h3)
+# heights.append(h4)
+# heights.append(h5)
+# c = 0
+# if h1>= 170 and h1 <= 180:
+#     c += 1
+# if h2>= 170 and h2 <= 180:
+#     c += 1
+# if h3>= 170 and h3 <= 180:
+#     c += 1
+# if h4>= 170 and h4 <= 180:
+#     c += 1
+# if h5>= 170 and h5 <= 180:
+#     c += 1
+# print(c)
+
+
+# x = []
+# n1 = input("enter a name: ")
+# x.append(n1[0])
+# n2 = input("enter a name: ")
+# x.append(n2[0])
+# c = 0
+# if x[0] == "a" or x[0] == "A":
+#     c += 1
+# if x[1] == "a" or x[1] == "A":
+#     c += 1
+# print(c)
+    
+scores = []
+s1 = float(input("enter score 1: "))
+s2 = float(input("enter score 2: "))
+s3 = float(input("enter score 3: "))
+s4 = float(input("enter score 4: "))
+scores.append(s1)
+scores.append(s2)
+scores.append(s3)
+scores.append(s4)
+print(scores)
+
+scores[0] += 2
+scores[1] += 2
+scores[2] += 2
+scores[3] += 2
+print(scores)
